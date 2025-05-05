13317238749dc3fa390f7ca590621907e034b434
Zach Button <zb7df@umsystem.edu>
2025-05-05 00:49:10 -0500

    Add wget commands

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 from datetime import date
+import torch
+import numpy as np
 
+# Set PyTorch seed
+myseed = 314
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(myseed)
+torch.manual_seed(myseed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(myseed)
 
 # General filepaths
 base_dir = '/content/drive/MyDrive/data/CS-5567'
