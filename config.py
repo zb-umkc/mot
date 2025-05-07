f69aeaa7fd884daa8d0c716d663d87cd65ece66d
zb-umkc <140571101+zb-umkc@users.noreply.github.com>
2025-05-07 00:04:07 -0500

    Update config.py

--- config.py
+++ config.py
@@ -20,13 +20,13 @@ today = date.today().strftime("%Y%m%d")
 # Detection model configurations
 ckpt_filename_det = f"{base_dir}/detection_model_20250501.pth"
 save_filename_det = f"{base_dir}/detection_model_{today}.pth"
-num_epochs_det = 4
+num_epochs_det = 10
 det_threshold = 0.7
 
 # Re-ID model configurations
 ckpt_filename_reid = f"{base_dir}/reid_model_20250425.pth"
 save_filename_reid = f"{base_dir}/reid_model_{today}.pth"
-num_epochs_reid = 4
+num_epochs_reid = 10
 reid_threshold = 0.7
 
 # Image resolutions
